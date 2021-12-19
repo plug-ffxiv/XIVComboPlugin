d784499361e5929737d1b539d0bbe4cae4008572
plug-ffxiv <96280754+plug-ffxiv@users.noreply.github.com>
2021-12-19 14:11:36 -0500

    Update BLM.cs

--- XIVComboExpanded/Combos/BLM.cs
+++ XIVComboExpanded/Combos/BLM.cs
@@ -290,13 +290,13 @@ namespace XIVComboExpandedPlugin.Combos
                 
                 var thundercloud = FindEffect(BLM.Buffs.Thundercloud);
                 
+                var cooldownData = GetCooldown(BLM.Sharpcast);
+                
                 if (thundercloud.RemainingTime <= 12 && HasEffect(BLM.Buffs.Sharpcast))
                     return BLM.Thunder3;
                 
                 if (gauge.InUmbralIce)
-                {
-                    var cooldownData = GetCooldown(BLM.Sharpcast);
-                
+                {                
                     if (!HasEffect(BLM.Buffs.Sharpcast) && (cooldownData.CooldownElapsed >= 30 || !cooldownData.IsCooldown))
                         return BLM.Sharpcast;
                     

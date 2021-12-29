e3a83fe94bde514a99d8f55b6709bac85b90985b
plug-ffxiv <96280754+plug-ffxiv@users.noreply.github.com>
2021-12-29 12:53:51 -0500

    Update AST.cs

--- XIVComboExpanded/Combos/AST.cs
+++ XIVComboExpanded/Combos/AST.cs
@@ -63,7 +63,7 @@ namespace XIVComboExpandedPlugin.Combos
 
                 if (IsEnabled(CustomComboPreset.AstrologianAstrodynePlayFeature))
                 {
-                    if (level >= AST.Levels.Astrodyne && !gauge.ContainsSeal(SealType.NONE) && (cooldownData.IsCooldown || !(gauge.DrawnCard == CardType.NONE))
+                    if (level >= AST.Levels.Astrodyne && !gauge.ContainsSeal(SealType.NONE) && (cooldownData.IsCooldown || !(gauge.DrawnCard == CardType.NONE)))
                         return AST.Astrodyne;
                 }
 
